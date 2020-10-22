d3cb3b16d81f80604c9928e1f1b2c4b93613567c
Ma <manchao@38f9d3587685.ant.amazon.com>
2020-10-22 18:46:43 +0800

    update

--- kvstore/pull.py
+++ kvstore/pull.py
@@ -35,7 +35,7 @@ def start_server(args):
     print("Create data done.")
     kvserver = dgl.distributed.KVServer(server_id=args.server_id,
                                         ip_config='ip_config.txt',
-                                        num_servers=num_servers,
+                                        num_servers=args.num_servers,
                                         num_clients=1)
 
     policy, gpb = create_range_partition_policy(args)
